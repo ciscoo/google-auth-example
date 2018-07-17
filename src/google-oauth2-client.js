2ee1ec4dea09dbcba74a10563c0ffa81ec2edfeb
ciscoo <cisco21c@gmail.com>
2018-07-16 19:05:24 -0500

    Update setup based on latest version of lib.

--- src/google-oauth2-client.js
+++ src/google-oauth2-client.js
@@ -1,10 +1,9 @@
-const GoogleAuth = require("google-auth-library");
+const { OAuth2Client } = require("google-auth-library");
 
-const { OAuth2 } = new GoogleAuth();
-const oauth2Client = new OAuth2(
+const oAuth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_CALLBACK_URL
 );
 
-module.exports = oauth2Client;
+module.exports = oAuth2Client;
